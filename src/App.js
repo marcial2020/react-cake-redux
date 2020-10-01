aca9427e0af2a1dd15415c20ab9d6353355a1941
marcial melong <s1270701@student.mcckc.edu>
2020-10-01 05:12:01 -0500

    add my commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import Products from './components/Products';
+import data from "./data.json";
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/"> React Cake Store</a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        All right reserved
-      </footer>
-      
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    };
+  }
+  render(){
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/"> React Cake Store</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}/>
+              </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All right reserved</footer>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
