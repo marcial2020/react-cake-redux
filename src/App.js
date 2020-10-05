0259f7d9d7fa37efecf1c5fb71a36ab3d9ada1a8
marcial melong <s1270701@student.mcckc.edu>
2020-10-05 12:03:14 -0500

    add new commit

--- src/App.js
+++ src/App.js
@@ -10,10 +10,14 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
-      cartItems: [],
+      cartItems: localStorage.getItem("cartItems")? JSON.parse(localStorage.getItem("cartItems")): [], //make the cart to hold item after refresh
       size: "",
-      sort: ""
+      sort: "",
     };
+  }
+
+  createOrder = (order) => {
+    alert("Need to save order for " + order.name);
   }
 
   removeFromCart = (product) => {
@@ -22,21 +26,27 @@ class App extends React.Component {
       cartItems: cartItems.filter((x) => x._id !== product._id)
     });
     
+    localStorage.setItem(
+      "cartItems",
+      JSON.stringify(cartItems.filter((x) => x._id !== product._id))
+    );  //make the cart to hold item after refresh
+
   }
 
   addToCart = (product) => {
     const cartItems = this.state.cartItems.slice();
     let alreadyInCart = false;
-    cartItems.forEach(item  => {
+    cartItems.forEach((item) => {
       if (item._id === product._id) {
         item.count++;
         alreadyInCart = true;
       }
     });
-    if(!alreadyInCart) {
-      cartItems.push({...product, count: 1});
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
     }
-    this.setState({cartItems});
+    this.setState({ cartItems });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems)); //make the cart to hold item after refresh
   }
 
   sortProducts = (event) => {
@@ -93,6 +103,7 @@ class App extends React.Component {
               <Cart 
               cartItems = {this.state.cartItems}
               removeFromCart = {this.removeFromCart}
+              createOrder={this.createOrder}
               />
             </div>
           </div>
